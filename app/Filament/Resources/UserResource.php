03ce83c0ea4c07bccee51cd20f8b02c02949b94b
FelixWahyu <felixwahyusejati89@gmail.com>
2025-06-05 20:05:07 +0700

    Add form input user and Resource kategori

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -6,6 +6,8 @@ use App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource\RelationManagers;
 use App\Models\User;
 use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
@@ -13,12 +15,13 @@ use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Hash;
 
 class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-users';
 
     protected static ?string $navigationLabel = 'Data Pengguna';
 
@@ -26,7 +29,23 @@ class UserResource extends Resource
     {
         return $form
             ->schema([
-                //
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('email')
+                    ->required(),
+                TextInput::make('password')
+                    ->required()
+                    ->password()
+                    ->dehydrated(fn($state) => filled($state))
+                    ->dehydrateStateUsing(fn($state) => Hash::make($state)),
+                Select::make('role_user')
+                    ->options([
+                        'kasir' => 'Kasir',
+                        'admin' => 'Admin',
+                        'superAdmin' => 'Super Admin'
+                    ])
+                    ->required()
+                    ->native(false)
             ]);
     }
 
